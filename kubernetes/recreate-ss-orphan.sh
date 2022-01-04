e83f4d9e7f33e41981c44ebd74420304e19420c5
Ludovit Scholtz <ludko2@gmail.com>
2022-01-04 20:19:37 +0100

    resize main drive

--- kubernetes/recreate-ss-orphan.sh
+++ kubernetes/recreate-ss-orphan.sh
@@ -1,2 +1,4 @@
 kubectl delete statefulset relaynode-ss -n algorand  --cascade=orphan
 kubectl apply -f statefulset.yaml
+
+# to reside .. delete with orphan, delete pod, resize in control panel, apply statefulset, ssh to server, df, resize2fs /dev/disk/.. repeat for each pod
