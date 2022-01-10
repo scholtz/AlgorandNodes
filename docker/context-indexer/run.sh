e3f46c7dd6a1aaacc54f9ae9a0bd54de3cecd98d
Ludovit Scholtz <ludko2@gmail.com>
2022-01-10 21:55:47 +0100

    indexer works

--- docker/context-indexer/run.sh
+++ docker/context-indexer/run.sh
@@ -1,6 +1,11 @@
 echo "starting indexer"
 date
 algorand-indexer import
+sleep 1
+algorand-indexer daemon
+sleep 1
+algorand-indexer import
+sleep 1
 algorand-indexer daemon
 
 while true; do echo "hello indexer" `date`; sleep 600;done
