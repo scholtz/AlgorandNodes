3cf613f6cf85fb1e8aa5e6cff73f9316eac539ad
Ludovit Scholtz <ludko2@gmail.com>
2022-03-03 11:55:00 +0100

    v 3.4.2

---
+++ docker/compose-relaynode-official-3.4.2-stable.sh
@@ -0,0 +1,3 @@
+ver=3.4.2
+docker build -t scholtz2/algorand-relay-mainnet:$ver-stable -f compose-relaynode-official.yaml --progress=plain --build-arg ALGO_VER=$ver context/
+docker push scholtz2/algorand-relay-mainnet:$ver-stable
