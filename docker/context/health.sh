82bf1c6f415b4038295ac8f914c0283620e556c7
Ludovit Scholtz <ludko2@gmail.com>
2022-05-11 21:03:34 +0200

    store external and internal outage check

--- docker/context/health.sh
+++ docker/context/health.sh
@@ -7,3 +7,5 @@ else
 fi
 
 # curl http://algo-set-service.algorand-mainnet:4160/v1/mainnet-v1.0/block/0 --output x.tmp
+# wget  -q -O /dev/stdout http://algo-set-service.algorand-mainnet:4160/v1/mainnet-v1.0/block/20000000 | sha256sum e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+# wget  -q -O /dev/stdout http://relay-fi.a-wallet.net:4160/v1/mainnet-v1.0/block/20000000 | sha256sum e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
