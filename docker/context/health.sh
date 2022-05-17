6ba0f95a10949d685721b34d0bf5f7110f9b7655
Ludovit Scholtz <ludko2@gmail.com>
2022-05-17 15:43:28 +0200

    jp to ver 3.6.2

--- docker/context/health.sh
+++ docker/context/health.sh
@@ -7,5 +7,5 @@ else
 fi
 
 # curl http://algo-set-service.algorand-mainnet:4160/v1/mainnet-v1.0/block/0 --output x.tmp
-# wget  -q -O /dev/stdout http://algo-set-service.algorand-mainnet:4160/v1/mainnet-v1.0/block/20000000 | sha256sum e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
-# wget  -q -O /dev/stdout http://relay-fi.a-wallet.net:4160/v1/mainnet-v1.0/block/20000000 | sha256sum e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
+# wget  -q -O /dev/stdout http://algo-set-service.algorand-mainnet:4160/v1/mainnet-v1.0/block/0 | sha256sum dd42911a7326348672638c5571d7cc4d6a62284b6d9c4b1307389a545f52a451
+# wget  -q -O /dev/stdout http://relay-fi.a-wallet.net:4160/v1/mainnet-v1.0/block/0 | sha256sum dd42911a7326348672638c5571d7cc4d6a62284b6d9c4b1307389a545f52a451
