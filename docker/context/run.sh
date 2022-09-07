b1b5d9403b38a86ca25e8f716af3c2e699fecd71
Scholtz <ludko2@gmail.com>
2022-09-07 12:51:35 +0200

    fix run

--- docker/context/run.sh
+++ docker/context/run.sh
@@ -1,18 +1,6 @@
 #apt update && apt dist-upgrade -y 
 
 
-{
-	"Version": 22,
-	"Archival": true,
-	"EnableMetricReporting": true,
-	"EnableLedgerService": true,
-	"EnableBlockService": true,
-	"DNSSecurityFlags": 0,
-    "DNSBootstrapID": "aramidmain.a-wallet.net",
-	"NetAddress": ":14260",
-	"EndpointAddress": "0.0.0.0:18080",
-    "FallbackDNSResolverAddress": "aramidmain.a-wallet.net"
-}
 goal node start 
 diagcfg telemetry enable
 diagcfg metric enable
