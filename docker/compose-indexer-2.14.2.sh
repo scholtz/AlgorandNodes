38cb993e8dc3d5c578425574f56d3ba89210c392
Scholtz <ludko2@gmail.com>
2022-11-13 13:19:38 +0100

    cicd update indexer from 2.14.2 to 2.15.0-rc1

--- docker/compose-indexer-2.14.2.sh
+++ docker/compose-indexer-2.14.2.sh
@@ -6,18 +6,18 @@ else
     # stable
     produceVer=$ver-stable
 fi
-echo $produceVer->$ver
-echo "docker build -t scholtz2/algorand-indexer:$ver-stable -f compose-indexer-stable.dockerfile --progress=plain --build-arg INDEXER_VER=$ver context-indexer/"
-docker build -t scholtz2/algorand-indexer:$ver-stable -f compose-indexer-stable.dockerfile --progress=plain --build-arg INDEXER_VER=$ver context-indexer/ || error_code=$?
+echo "$produceVer->$ver"
+echo "docker build -t scholtz2/algorand-indexer:$produceVer -f compose-indexer-stable.dockerfile --progress=plain --build-arg INDEXER_VER=$ver context-indexer/"
+docker build -t scholtz2/algorand-indexer:$produceVer -f compose-indexer-stable.dockerfile --progress=plain --build-arg INDEXER_VER=$ver context-indexer/ || error_code=$?
 error_code_int=$(($error_code + 0))
 if [ $error_code_int -ne 0 ]; then
     echo "failed to build image";
 	exit 1;
 fi
-docker push scholtz2/algorand-indexer:$ver-stable || error_code=$?
+docker push scholtz2/algorand-indexer:$produceVer || error_code=$?
 error_code_int=$(($error_code + 0))
 if [ $error_code_int -ne 0 ]; then
     echo "failed to push image";
 	exit 1;
 fi
-echo "scholtz2/algorand-indexer:$ver-stable"
+echo "scholtz2/algorand-indexer:$produceVer"
