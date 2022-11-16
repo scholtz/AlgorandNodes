8d2621c2ef0400a189ba5f0212a2d1584d14f73a
Scholtz <ludko2@gmail.com>
2022-11-16 19:09:54 +0100

    cicd update from 3.11.2 to 3.11.3

---
+++ docker/compose-kmd-official-3.11.3-stable.sh
@@ -0,0 +1,17 @@
+ver=3.11.3
+docker build -t scholtz2/algorand-kmd-mainnet:$ver-stable -f compose-kmd-official.dockerfile --progress=plain --build-arg ALGO_VER=$ver context-kmd/ || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build image";
+	exit 1;
+fi
+docker push scholtz2/algorand-kmd-mainnet:$ver-stable || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to push image";
+	exit 1;
+fi
+docker tag scholtz2/algorand-kmd-mainnet:$ver-stable scholtz2/algorand-participation-mainnet:$ver-stable
+docker push scholtz2/algorand-participation-mainnet:$ver-stable
+
+#docker build -t scholtz2/algorand-participation-sandbox:$ver-stable -f compose-participation-sandbox.dockerfile --progress=plain --build-arg ALGO_VER=$ver context-participation-sandbox/
