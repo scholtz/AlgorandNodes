ee55561a7c138da70c3696652cec6ceb25f32fae
Scholtz <ludko2@gmail.com>
2022-12-19 20:17:40 +0100

    aramid to 3.12.2

--- docker/compose-aramid.dockerfile
+++ docker/compose-aramid.dockerfile
@@ -1,9 +1,9 @@
 ARG ALGO_VER
 
 FROM scholtz2/algorand-relay-mainnet:$ALGO_VER
-USER root
-
-COPY . . 
-RUN chmod 0777 *.sh
-
 USER algo
+COPY . . 
+USER root
+RUN chmod 0777 *.sh
+USER algo
+CMD /app/run.sh
--- docker/context-aramid/health.sh
+++ docker/context-aramid/health.sh
@@ -2,8 +2,10 @@ check="Sync Time: 0.0s"
 result=`goal node status | grep "$check"`
 if [ "$result" = "$check" ]; then
    echo "0"
+   exit 0
 else
    echo "1"
+   exit 1
 fi
 
 # curl http://algo-set-service.algorand-mainnet:4160/v1/mainnet-v1.0/block/0 --output x.tmp
