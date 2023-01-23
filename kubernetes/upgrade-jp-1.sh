76efaefcecab228d0443858ea6619f05dbd167d8
Ludovit Scholtz <ludko2@gmail.com>
2023-01-23 12:24:44 +0100

    jp release, sanbox indexer to 2.15.2

---
+++ kubernetes/upgrade-jp-1.sh
@@ -0,0 +1,10 @@
+cd mainnet-relay-jp
+echo "kubectl apply -f h1-deployment.yaml"
+date
+kubectl apply -f h1-deployment.yaml
+sleep 1
+echo "waiting for pods to be live"
+while kubectl get deployment relaynode-1 -n algo-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+
+date
+echo "DONE"
