3815fec788a0b9ca088f0f0e861430581163cb1f
Scholtz <ludko2@gmail.com>
2023-02-14 17:02:40 +0100

    tune upgrade de

--- kubernetes/upgrade-de.sh
+++ kubernetes/upgrade-de.sh
@@ -14,4 +14,32 @@ echo "waiting for pods to be live"
 while kubectl get deployment relaynode-2 -n algo-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
 
 date
+echo "DONE ALGO RELAY DE, Upgrading Participation KMD DE"
+
+cd ../kmd-de
+echo "kubectl apply -f h2-deployment.yaml"
+date
+kubectl apply -f h2-deployment.yaml
+sleep 1
+echo "waiting for pods to be live"
+while kubectl get deployment kmd-node-2 -n algo-kmd-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+
+date
+echo "DONE Upgrading Participation KMD DE, Upgrading aramid servers"
+cd ../aramid-main-de
+
+date
+kubectl apply -f h1-deployment.yaml
+sleep 1
+echo "waiting for pods to be live"
+while kubectl get deployment relaynode-1 -n aramid-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+
+date
+echo "kubectl apply -f h2-deployment.yaml"
+kubectl apply -f h2-deployment.yaml
+sleep 1
+echo "waiting for pods to be live"
+while kubectl get deployment relaynode-2 -n aramid-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+
+date
 echo "DONE"
