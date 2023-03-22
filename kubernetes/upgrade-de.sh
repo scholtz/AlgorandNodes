2dd2845e72bfe62c348701f4dfe6b48097eb0804
Scholtz <ludko2@gmail.com>
2023-03-22 20:19:44 +0100

    add g3 to upgrade-de.sh

--- kubernetes/upgrade-de.sh
+++ kubernetes/upgrade-de.sh
@@ -14,6 +14,13 @@ echo "waiting for pods to be live"
 while kubectl get deployment relaynode-2 -n algo-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
 
 date
+echo "kubectl apply -f g3-deployment.yaml"
+kubectl apply -f g3-deployment.yaml
+sleep 1
+echo "waiting for pods to be live"
+while kubectl get deployment relaynode-3 -n algo-relay-mainnet-tmp -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+
+date
 echo "DONE ALGO RELAY DE, Upgrading Participation KMD DE"
 
 cd /home/scholtz/AlgorandNodes/kubernetes/algod-participation/mainnet-participation/de-1-participation
