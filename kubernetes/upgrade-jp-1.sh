413ace6ea03b4c6099b71878411d97799e446ead
Scholtz <ludko2@gmail.com>
2023-04-04 16:15:58 +0200

    telemetry test

--- kubernetes/upgrade-jp-1.sh
+++ kubernetes/upgrade-jp-1.sh
@@ -1,10 +1,10 @@
 cd /home/scholtz/AlgorandNodes/kubernetes/algod-relay/mainnet-relay/jp-1-mainnet-relay
 echo "kubectl apply -f statefulset.yaml"
-# date
-# kubectl apply -f statefulset.yaml
-# sleep 1
-# echo "waiting for pods to be live"
-# while kubectl get statefulset relaynode-ss -n algorand -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+date
+kubectl apply -f statefulset.yaml
+sleep 1
+echo "waiting for pods to be live"
+while kubectl get statefulset relaynode-ss -n algorand -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
 
 date
 echo "DONE"
