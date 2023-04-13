1a783862693969420be49bbdc5d4a5d25a97f6c5
Scholtz <ludko2@gmail.com>
2023-04-13 17:07:41 +0200

    cicd update from 3.15.0 to 3.15.1

---
+++ docker/algod-relay/testnet/compose-relaynode-official-3.15.1-stable-testnet.sh
@@ -0,0 +1,14 @@
+ver=3.15.1
+docker build -t scholtz2/algorand-relay-testnet:$ver-stable -f compose-relaynode-official-testnet.dockerfile --progress=plain --build-arg ALGO_VER=$ver context-testnet/ || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build";
+	exit 1;
+fi
+
+docker push scholtz2/algorand-relay-testnet:$ver-stable || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to push";
+	exit 1;
+fi
