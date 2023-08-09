3dc833998d2b6483b88650c961a42a1f2db4bbca
Scholtz <ludko2@gmail.com>
2023-08-09 17:07:44 +0200

    cicd update from 3.16.3 to 3.17.0

---
+++ docker/algod-relay/mainnet/compose-relaynode-official-null-stable.sh
@@ -0,0 +1,13 @@
+ver=null
+docker build -t scholtz2/algorand-relay-mainnet:$ver-stable -f compose-relaynode-official.dockerfile --progress=plain --build-arg ALGO_VER=$ver context/ || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build";
+	exit 1;
+fi
+docker push scholtz2/algorand-relay-mainnet:$ver-stable || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to push";
+	exit 1;
+fi
