3f25b54cb0b00737e8b512b1fe8aa3cefb8521f5
Ludovit Scholtz <ludko2@gmail.com>
2023-09-30 23:14:02 +0200

    update voi

--- kubernetes/upgrade-fi.sh
+++ kubernetes/upgrade-fi.sh
@@ -43,3 +43,23 @@ while kubectl get deployment relaynode-2 -n aramid-relay-mainnet -ojson | jq '.s
 
 date
 echo "DONE"
+
+echo "DONE Upgrading VoiTest relay"
+cd /home/scholtz/AlgorandNodes/kubernetes/algod-relay/voitest-relay/fi-1-voitest-relay
+
+date
+kubectl apply -f s3-k1-fi-deployment.yaml
+sleep 1
+echo "waiting for pods to be live"
+while kubectl get deployment relaynode-3 -n algo-relay-voitest -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+
+date
+echo "DONE"
+
+echo "DONE Upgrading VoiTest public participation server"
+cd /home/scholtz/AlgorandNodes/kubernetes/algod-participation/voitest-participation/fi-1-participation
+date
+kubectl apply -f h2-deployment.yaml
+sleep 1
+echo "waiting for pods to be live"
+while kubectl get deployment participation-node-2 -n algo-participation-voitest -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
