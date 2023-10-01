47eb758f1366bcb9b94990a05c8c66e08631e549
Scholtz <ludko2@gmail.com>
2023-10-01 16:59:35 +0200

    fix health probe

--- docker/algod-relay/testnet/context-testnet/health.sh
+++ docker/algod-relay/testnet/context-testnet/health.sh
@@ -2,8 +2,8 @@ check="Sync Time: 0.0s"
 result=`goal node status | grep "$check"`
 if [ "$result" = "$check" ]; then
    echo "0"
+   exit 0
 else
    echo "1"
+   exit 1
 fi
-
-# curl http://algo-set-service.algorand-testnet:4161/v1/testnet-v1.0/block/0 --output x.tmp
