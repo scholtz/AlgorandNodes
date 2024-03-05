532ef22056a60a9c8808ab2687a31a4397aefa36
Scholtz <ludko2@gmail.com>
2024-03-05 18:34:46 +0100

    cicd update from 3.22.0 to 3.22.1

---
+++ docker/algod-participation/sandbox/compose-participation-sandbox-3.22.1-stable.sh
@@ -0,0 +1,13 @@
+ver=3.22.1
+docker build -t scholtz2/algorand-participation-sandbox:$ver-stable -f compose-participation-sandbox.dockerfile --build-arg ALGO_VER=$ver context-participation-sandbox/ || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build";
+	exit 1;
+fi
+docker push scholtz2/algorand-participation-sandbox:$ver-stable || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to push";
+	exit 1;
+fi
--- kubernetes/upgrade-de.sh
+++ kubernetes/upgrade-de.sh
@@ -31,22 +31,22 @@ sleep 1
 echo "waiting for pods to be live"
 while kubectl get deployment kmd-node-2 -n algo-kmd-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
 
-date
-echo "DONE Upgrading Participation KMD DE, Upgrading aramid servers"
-cd /home/scholtz/AlgorandNodes/kubernetes/algod-relay/aramid-relay/de-1-aramid-relay
+# date
+# echo "DONE Upgrading Participation KMD DE, Upgrading aramid servers"
+# cd /home/scholtz/AlgorandNodes/kubernetes/algod-relay/aramid-relay/de-1-aramid-relay
 
-date
-kubectl apply -f h1-deployment.yaml
-sleep 1
-echo "waiting for pods to be live"
-while kubectl get deployment relaynode-1 -n aramid-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+# date
+# kubectl apply -f h1-deployment.yaml
+# sleep 1
+# echo "waiting for pods to be live"
+# while kubectl get deployment relaynode-1 -n aramid-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
 
-date
-echo "kubectl apply -f h2-deployment.yaml"
-kubectl apply -f h2-deployment.yaml
-sleep 1
-echo "waiting for pods to be live"
-while kubectl get deployment relaynode-2 -n aramid-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+# date
+# echo "kubectl apply -f h2-deployment.yaml"
+# kubectl apply -f h2-deployment.yaml
+# sleep 1
+# echo "waiting for pods to be live"
+# while kubectl get deployment relaynode-2 -n aramid-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
 
 date
 echo "DONE"
