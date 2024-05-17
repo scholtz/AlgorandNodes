16bf5dc7e2d06ea962175ca794b9ee2c39b71fb5
Scholtz <ludko2@gmail.com>
2024-05-18 00:41:00 +0200

    move in relays to non archival

--- kubernetes/upgrade-in.sh
+++ kubernetes/upgrade-in.sh
@@ -1,24 +1,24 @@
-cd /home/scholtz/AlgorandNodes/kubernetes/algod-relay/mainnet-relay/in-1-mainnet-relay
-echo "kubectl apply -f h1-deployment.yaml"
+cd /home/scholtz/AlgorandNodes/kubernetes/algod-relay/mainnet-relay/in-2-mainnet-relay
+echo "kubectl apply -f h4-deployment.yaml"
 date
-kubectl apply -f h1-deployment.yaml
+kubectl apply -f h4-deployment.yaml
 sleep 1
 echo "waiting for pods to be live"
-while kubectl get deployment relaynode-1 -n algo-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+while kubectl get deployment relaynode-4 -n algo-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
 
 date
-echo "kubectl apply -f h2-deployment.yaml"
-kubectl apply -f h2-deployment.yaml
+echo "kubectl apply -f h5-deployment.yaml"
+kubectl apply -f h5-deployment.yaml
 sleep 1
 echo "waiting for pods to be live"
-while kubectl get deployment relaynode-2 -n algo-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+while kubectl get deployment relaynode-5 -n algo-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
 
 date
-echo "kubectl apply -f h3-deployment.yaml"
-kubectl apply -f h3-deployment.yaml
+echo "kubectl apply -f h6-deployment.yaml"
+kubectl apply -f h6-deployment.yaml
 sleep 1
 echo "waiting for pods to be live"
-while kubectl get deployment relaynode-3 -n algo-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
+while kubectl get deployment relaynode-6 -n algo-relay-mainnet -ojson | jq '.status.conditions[].status' -r | grep -q False; do sleep 1; date; done
 
 date
 echo "DONE"
