e6e062a2a0692198ec9e938f2c0644cf284afb8d
Scholtz <ludko2@gmail.com>
2024-05-20 17:11:30 +0200

    cicd update from 3.23.1 to 3.24.0

---
+++ docker/algod-relay/aramidmain/aramid-from-official-3.24.0-stable.sh
@@ -0,0 +1,14 @@
+ver=3.24.0-stable
+outputVer=v3.24.0-stable
+docker build -t scholtz2/aramid-algo-node:$outputVer -f compose-aramid.dockerfile --build-arg ALGO_VER=$ver context-aramid/ || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build image";
+	exit 1;
+fi
+docker push scholtz2/aramid-algo-node:$outputVer || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to push image";
+	exit 1;
+fi
---
+++ docker/algod-relay/voitest/compose-relaynode-official-3.24.0-stable-testnet.sh
@@ -0,0 +1,14 @@
+ver=3.24.0
+docker build -t scholtz2/algorand-relay-voitest:$ver-stable -f compose-relaynode-official-testnet.dockerfile --build-arg ALGO_VER=$ver context-testnet/ || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build";
+	exit 1;
+fi
+
+docker push scholtz2/algorand-relay-voitest:$ver-stable || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to push";
+	exit 1;
+fi
