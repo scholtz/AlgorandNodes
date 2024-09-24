f98f7d684d0cab82d877e6dadc169b38219dcdf9
Scholtz <ludko2@gmail.com>
2024-09-24 18:40:47 +0200

    update aramid relay default config

--- docker/algod-relay/aramidmain/context-aramid/run.sh
+++ docker/algod-relay/aramidmain/context-aramid/run.sh
@@ -13,6 +13,24 @@ fi
 
 sleep 1
 
+myNodeRound=`goal node lastround`
+blockchainRoundMath=$(curl -s https://algod.aramidmain.a-wallet.net/v2/status | jq -r '.["last-round"]' )" - 1000"
+blockchainRound=$(echo $blockchainRoundMath | bc)
+
+echo "catchup check: my round is $myNodeRound, blockchain round is $blockchainRound"
+if [ "$myNodeRound" -lt "$blockchainRound" ]; then
+	echo "Executing catchup script because my current round is $myNodeRound"
+	catchup=$(curl -s https://algod.aramidmain.a-wallet.net/v2/status | jq -r '.["last-catchpoint"]')
+	sleep 10s
+	echo "going to run 'goal node catchup $catchup'"
+	goal node catchup $catchup || error_code=$?
+	if [ $error_code_int -ne 0 ]; then
+		echo "failed to start catchup";
+		exit 1;
+	fi
+fi
+
+
 max_retry=$((60 + 0))
 counter=$((0 + 0))
 until /app/health.sh
@@ -25,7 +43,7 @@ do
    fi
    ((counter++))
 done
-
+chmod 0700 /app/data/kmd-v0.5
 goal kmd start  || error_code=$?
 error_code_int=$(($error_code + 0))
 if [ $error_code_int -ne 0 ]; then
