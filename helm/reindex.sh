9ab29c9f92c0623eab5952b8fb24370e4065ebb7
Scholtz <ludko2@gmail.com>
2024-10-28 12:07:29 +0100

    helm voimain participation tuning

---
+++ helm/reindex.sh
@@ -0,0 +1,6 @@
+
+helm package aramid-relay/
+helm package aramid-participation/
+helm package voimain-participation/
+helm package aramid-indexer/
+helm repo index --url https://scholtz.github.io/AlgorandNodes/helm/ .
