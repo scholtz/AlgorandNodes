e8e51f37383357005a08e648c61d636daaa7dbaf
Scholtz <ludko2@gmail.com>
2025-07-17 08:56:27 +0200

    sync

--- kubernetes/algod-participation/voimain-participation/de-1-participation/install.sh
+++ kubernetes/algod-participation/voimain-participation/de-1-participation/install.sh
@@ -2,3 +2,5 @@ helm repo add biatec-repo https://scholtz.github.io/AlgorandNodes/helm/
 helm repo update
 helm upgrade --install voimain-participation biatec-repo/voimain-participation -n voimain-participation --create-namespace --values ./p-1-values.yaml
 helm upgrade --install voimain-participation biatec-repo/voimain-participation -n voimain-participation-2 --create-namespace --values ./p-2-values.yaml
+
+helm upgrade --install voimain-participation biatec-repo/voimain-participation -n voimain-participation --create-namespace --values ./p-3-values.yaml
