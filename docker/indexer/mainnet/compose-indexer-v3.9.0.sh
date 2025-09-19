ad04b78d5e42996e41e123dcdab80f7417c96639
Scholtz <ludko2@gmail.com>
2025-09-19 18:22:40 +0200

    cicd update indexer from v3.7.2 to v3.9.0

---
+++ docker/indexer/mainnet/compose-indexer-v3.9.0.sh
@@ -0,0 +1,23 @@
+ver=v3.9.0
+if [[ "$ver" == *"-rc"* ]]; then
+    # not stable
+    produceVer=$ver
+else
+    # stable
+    produceVer=$ver-stable
+fi
+echo "$produceVer->$ver"
+echo "docker build -t scholtz2/algorand-indexer:$produceVer -f compose-indexer-stable.dockerfile --build-arg INDEXER_VER=$ver context-indexer/"
+docker build -t scholtz2/algorand-indexer:$produceVer -f compose-indexer-stable.dockerfile --build-arg INDEXER_VER=$ver context-indexer/ || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to build image";
+	exit 1;
+fi
+docker push scholtz2/algorand-indexer:$produceVer || error_code=$?
+error_code_int=$(($error_code + 0))
+if [ $error_code_int -ne 0 ]; then
+    echo "failed to push image";
+	exit 1;
+fi
+echo "scholtz2/algorand-indexer:$produceVer"
