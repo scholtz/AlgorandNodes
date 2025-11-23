7a842b3a741f380da0aa28b451d5353606f2555f
Ludovit Scholtz <ludko2@gmail.com>
2025-11-23 12:40:10 +0100

    Add Helm repository for aramid-participation

--- kubernetes/algod-participation/aramid-participation/de-4-aramid-participation/install.sh
+++ kubernetes/algod-participation/aramid-participation/de-4-aramid-participation/install.sh
@@ -1,1 +1,4 @@
+helm repo add biatec-repo https://scholtz.github.io/AlgorandNodes/helm/
+helm repo update
+
 helm upgrade --install --create-namespace aramid-participation biatec-repo/aramid-participation --values ./p1.yaml --namespace aramid
