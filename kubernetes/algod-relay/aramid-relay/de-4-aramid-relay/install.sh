ee73b1d45beecc19c3eb0b2ea6ff2c489b98cd50
Ludovit Scholtz <ludko2@gmail.com>
2025-11-23 12:37:35 +0100

    Add Helm repo and update commands in install.sh

--- kubernetes/algod-relay/aramid-relay/de-4-aramid-relay/install.sh
+++ kubernetes/algod-relay/aramid-relay/de-4-aramid-relay/install.sh
@@ -1,3 +1,6 @@
+helm repo add biatec-repo https://scholtz.github.io/AlgorandNodes/helm/
+helm repo update
+
 helm upgrade --install --create-namespace aramid-relay biatec-repo/aramid-relay --values ./r1.yaml --namespace aramid
 
 helm upgrade --install --create-namespace aramid-relay biatec-repo/aramid-relay --values ./a1.yaml --namespace aramid
